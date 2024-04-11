4a04f78d9b6483331204969187023ebae50ae629
nathanbuyr <G00421082@atu.ie>
2024-04-11 15:21:12 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar,IonCardContent, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { MovieService } from '../Services/movie.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle,IonCardContent, IonContent, CommonModule, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit{
+  movies:any = [];
+  constructor(private movieService:MovieService) {}
+
+ngOnInit(): void {
+    this.movieService.GetMovieData().subscribe(
+      (data)=>{
+        this.movies = data.Search;
+      }
+    );
+}
+
 }
